1e4c9d68edea48e3e025e164f3c3c8a5129d1367
Sahil <beraSahil2004@gmail.com>
2025-11-04 21:16:58 +0530

    first commit

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -2,35 +2,35 @@ import React, { useEffect } from 'react';
 import './Home.css';
 import { GiBurningBook } from "react-icons/gi";
 import { Link } from 'react-router-dom';
-// import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
 
 const Home = () => {
-  // useEffect(() => {
-  //   // const fetchUsername = async () => {
-  //   //   const id = sessionStorage.getItem('id');
-  //   //   if (!id) return; // not logged in
+  useEffect(() => {
+    const fetchUsername = async () => {
+      const id = sessionStorage.getItem('id');
+      if (!id) return; // not logged in
 
-  //   //   try {
-  //   //     const res = await axios.get(`${window.location.origin}/api/v2/getUser/${id}`);
-  //   //     if (res.data && res.data.user) {
-  //   //       const username = res.data.user.username;
-  //   //       // sessionStorage.setItem('username', username);
+      try {
+        const res = await axios.get(`${window.location.origin}/api/v2/getUser/${id}`);
+        if (res.data && res.data.user) {
+          const username = res.data.user.username;
+          // sessionStorage.setItem('username', username);
 
-  //   //       toast.success(`Welcome back, ${username}!`, {
-  //   //         position: 'top-center',
-  //   //         autoClose: 2000,
-  //   //         theme: 'colored'
-  //   //       });
-  //   //     }
-  //   //   } catch (err) {
-  //   //     console.error('Error fetching username:', err);
-  //   //   }
-  //   // };
+          toast.success(`Welcome back, ${username}!`, {
+            position: 'top-center',
+            autoClose: 2000,
+            theme: 'colored'
+          });
+        }
+      } catch (err) {
+        console.error('Error fetching username:', err);
+      }
+    };
 
-  //   // fetchUsername();
-  // }, []);
+    fetchUsername();
+  }, []);
 
   return (
     <div className="home d-flex justify-content-center align-items-center">
